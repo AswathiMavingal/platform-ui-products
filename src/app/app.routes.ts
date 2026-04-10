8f68e00e4c74a162d50be1dab3d4a2dc358e15c2
aswathimavingal <aswathimavingal@gmail.com>
2026-04-10 16:35:21 +0530

    add product routes
    Signed-off-by: aswathimavingal <aswathimavingal@gmail.com>

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,5 +3,15 @@ import { App } from './app';
 
 export const routes: Routes = [ {
     path: '',
-    component: App
+    loadComponent: () => import('./app').then(m => m.App),
+    children: [
+        {
+            path: '',
+            loadComponent: () => import('./pages/product-list/product-list').then(m => m.ProductList)
+        },
+        {
+            path: 'details/:id',
+            loadComponent: () => import('./pages/details/details').then(m => m.Details)
+        }
+    ]
   }];
